cf83595e1bc20d2af80d442c0bdfe30d8c301312
Nora Zheng <zhengnuo@gmail.com>
2019-07-22 03:34:15 -0400

    Inital commit

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	//fetch a random joke
+	$(`button`).on('click', () => {
+		$.ajax({
+			url: 'https://icanhazdadjoke.com/',
+			method: 'GET',
+			dataType: 'json'
+		}).then(res => {
+			$(`div.randomJoke`).empty();
+			$(`div.randomJoke`)
+				.append(`<p>${res.joke}</p>`)
+				.hide()
+				.fadeIn(300);
+		});
+	});
+
+	//search joke by keyword
+	$(`input[type='submit']`).on('click', event => {
+		event.preventDefault();
+		$.ajax({
+			url: 'https://icanhazdadjoke.com/search',
+			method: 'GET',
+			dataType: 'json',
+			data: {
+				term: $(`input[type=text]`).val()
+			}
+		})
+			.then(res => {
+				if (res.results.length == 0) {
+					$(`ul.searchResults`).empty();
+					$(`ul.searchResults`)
+						.append(
+							`<p>Sorry! That keyword returned no result ಠ_ಠ; </p><p>Try another keyword (e.g. banana, pizza, light, guitar)</p>`
+						)
+						.hide()
+						.fadeIn(300);
+				} else {
+					$(`ul.searchResults`).empty();
+					res.results.map(result => {
+						console.log(result);
+						$(`ul.searchResults`)
+							.append(`<li>${result.joke}</li>`)
+							.hide()
+							.fadeIn(300);
+					});
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	});
+});
