37273ed0d721bc71bdb05c366382cad2e4bbfb6c
Nora Zheng <zhengnuo@gmail.com>
2019-10-04 01:28:40 -0400

    namespacing

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 const app = {};
-app.randomButton = document.querySelector('#randomButton');
-app.randomJokeDiv = document.querySelector('#randomJoke');
-app.resultsList = document.querySelector('#searchResults');
 
-document.addEventListener('DOMContentLoaded', function() {
-	//fetch a random joke
-
-	app.randomButton.addEventListener('click', () => {
-		$.ajax({
-			url: 'https://icanhazdadjoke.com/',
-			method: 'GET',
-			dataType: 'json'
-		}).then(res => {
-			app.randomJokeDiv.innerHTML = `<p class="fadeIn">${res.joke}</p>`;
-		});
+app.fetchRandomJoke = () => {
+	$.ajax({
+		url: 'https://icanhazdadjoke.com/',
+		method: 'GET',
+		dataType: 'json'
+	}).then(res => {
+		document.querySelector(
+			'#randomJoke'
+		).innerHTML = `<p class="fadeIn">${res.joke}</p>`;
 	});
+};
 
-	//search joke by keyword
-	$(`input[type='submit']`).on('click', event => {
-		event.preventDefault();
-		$.ajax({
-			url: 'https://icanhazdadjoke.com/search',
-			method: 'GET',
-			dataType: 'json',
-			data: {
-				term: $(`input[type=text]`).val()
+app.searchJoke = event => {
+	event.preventDefault();
+
+	const searchTerm = document.querySelector('#search').value;
+	const resDiv = document.querySelector('#results');
+	const resList = document.querySelector('#resultsList');
+
+	$.ajax({
+		url: 'https://icanhazdadjoke.com/search',
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			term: searchTerm
+		}
+	})
+		.then(res => {
+			if (res.results.length == 0) {
+				resDiv.innerHTML = `<p class="fadeIn">Sorry! That keyword returned no result ಠ_ಠ; </p><p class="fadeIn">Try another keyword (e.g. banana, pizza, light, guitar)</p>`;
+			} else {
+				resList.innerHTML = '';
+				res.results.map(result => {
+					const joke = document.createElement('li');
+					joke.innerHTML = `${result.joke}`;
+					resList.appendChild(joke);
+				});
 			}
 		})
-			.then(res => {
-				if (res.results.length == 0) {
-					app.resultsList.innerHTML = `<p class="fadeIn">Sorry! That keyword returned no result ಠ_ಠ; </p><p class="fadeIn">Try another keyword (e.g. banana, pizza, light, guitar)</p>`;
-				} else {
-					app.resultsList.innerHTML = '';
-					res.results.map(result => {
-						const joke = document.createElement('li');
-						joke.innerHTML = `${result.joke}`;
-						app.resultsList.appendChild(joke);
-					});
-				}
-			})
-			.catch(error => {
-				app.resultsList.innerHTML = `<p>Sorry! The server is down. </p><p>Try again later.</p>`;
-			});
-	});
+		.catch(() => {
+			resDiv.innerHTML = `<p>Sorry! The server is down. </p><p>Try again later.</p>`;
+		});
+};
+
+app.init = () => {
+	document
+		.querySelector('#fetchRandom')
+		.addEventListener('click', app.fetchRandomJoke);
+
+	document.querySelector('#submit').addEventListener('click', app.searchJoke);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+	app.init();
 });
