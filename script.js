4bdbff82a9409f7a9cb57d3ed29bf1b7404316a0
Nora Zheng <zhengnuo@gmail.com>
2019-10-03 22:33:11 -0400

    rewrote all jQuery to vanilla JS except for $.ajax

--- script.js
+++ script.js
@@ -1,62 +1,46 @@
-$(document).ready(function() {
-  //fetch a random joke
-  $(`button`).on('click', () => {
-    $.ajax({
-      url: 'https://icanhazdadjoke.com/',
-      method: 'GET',
-      dataType: 'json'
-    }).then(res => {
-      $(`div.randomJoke`).empty();
-      $(`div.randomJoke`)
-        .append(`<p>${res.joke}</p>`)
-        .hide()
-        .fadeIn(300);
-    });
-  });
+const app = {};
+app.randomButton = document.querySelector('#randomButton');
+app.randomJokeDiv = document.querySelector('#randomJoke');
+app.resultsList = document.querySelector('#searchResults');
 
-  //search joke by keyword
-  $(`input[type='submit']`).on('click', event => {
-    event.preventDefault();
-    $.ajax({
-      url: 'https://icanhazdadjoke.com/search',
-      method: 'GET',
-      dataType: 'json',
-      data: {
-        term: $(`input[type=text]`).val()
-      }
-    })
-      .then(res => {
-        if (res.results.length == 0) {
-          $(`ul.searchResults`).empty();
-          $(`ul.searchResults`)
-            .append(
-              `<p>Sorry! That keyword returned no result ಠ_ಠ; </p><p>Try another keyword (e.g. banana, pizza, light, guitar)</p>`
-            )
-            .hide()
-            .fadeIn(300);
-        } else {
-          $(`ul.searchResults`).empty();
-          res.results.map(result => {
-            console.log(result);
-            $(`ul.searchResults`)
-              .append(`<li>${result.joke}</li>`)
-              .hide()
-              .fadeIn(300);
-          });
-        }
-      })
-      .catch(error => {
-        $(`ul.searchResults`).empty();
-        $(`ul.searchResults`)
-          .append(`<p>Sorry! The server is down. </p><p>Try again later.</p>`)
-          .hide()
-          .fadeIn(300);
-      });
-    $('html, body').animate(
-      {
-        scrollTop: $('#searchResults').offset().top
-      },
-      550
-    );
-  });
+document.addEventListener('DOMContentLoaded', function() {
+	//fetch a random joke
+
+	app.randomButton.addEventListener('click', () => {
+		$.ajax({
+			url: 'https://icanhazdadjoke.com/',
+			method: 'GET',
+			dataType: 'json'
+		}).then(res => {
+			app.randomJokeDiv.innerHTML = `<p class="fadeIn">${res.joke}</p>`;
+		});
+	});
+
+	//search joke by keyword
+	$(`input[type='submit']`).on('click', event => {
+		event.preventDefault();
+		$.ajax({
+			url: 'https://icanhazdadjoke.com/search',
+			method: 'GET',
+			dataType: 'json',
+			data: {
+				term: $(`input[type=text]`).val()
+			}
+		})
+			.then(res => {
+				if (res.results.length == 0) {
+					app.resultsList.innerHTML = `<p class="fadeIn">Sorry! That keyword returned no result ಠ_ಠ; </p><p class="fadeIn">Try another keyword (e.g. banana, pizza, light, guitar)</p>`;
+				} else {
+					app.resultsList.innerHTML = '';
+					res.results.map(result => {
+						const joke = document.createElement('li');
+						joke.innerHTML = `${result.joke}`;
+						app.resultsList.appendChild(joke);
+					});
+				}
+			})
+			.catch(error => {
+				app.resultsList.innerHTML = `<p>Sorry! The server is down. </p><p>Try again later.</p>`;
+			});
+	});
 });
